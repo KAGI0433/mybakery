6c5037cc1ed9d8c03f2080a883576f54e314efee
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-09 21:05:04 +0200

    commit

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -6,6 +6,7 @@ export const Navigation = () => {
         <nav>
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
+          <Link to= '/menu'>Menu</Link>
         </nav>
     )
 }

455bb20e8d5e89425da54be5239422362f5e9db3
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-10 11:33:47 +0200

    home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,24 @@
 
+
+import React from 'react'
+import "./Home.css"
+import { MdDoubleArrow } from "react-icons/md";
 export const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>this is home component</p>
+        <div className='main mx-[320px] mt-[25px]'>
+            
+            <div className='content-section'>
+            <h2>MINNIE BAKES</h2>
+            <h1>FRESH BAKES</h1>
+            <h3>EVERYDAY</h3>
+                <div className='shop-now-btn '>
+                   <button>Shop now!</button>
+                   <MdDoubleArrow />
+                </div>
+                </div>
+                
         </div>
-    )
-}
+        
+    );
+};
+

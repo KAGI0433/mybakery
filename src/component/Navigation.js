a0390a066d957662cb3a558ff3eaac9559efe5ba
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-10 15:55:51 +0200

    commit

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const Navigation = () => {
     return (
         <nav>
-          <img src="" alt="" className="logo" />
+          
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
           <Link to= '/menu'>Menu</Link>

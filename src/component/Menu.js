a5ccd7db052fe5483bc2dc6ae91c368978af3562
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-11 15:12:01 +0200

    commit

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,14 +1,14 @@
 import React from "react"
 import MenuItem from'./MenuItem'
 import MenuList from '../helpers/MenuList'
-
+import './Menu.css'
 
   export const Menu =()  => {
     return (
         <>
-        <h1 className="text-center mt-3">Our Menu</h1>
+        <h1 className="menu">Our Menu</h1>
         <section className="py-4 container">
-            <div className="row justify-content-center">
+            <div className="card">
                  {MenuList.productDate.map((item, index) =>{
                        return(
                         <MenuItem img={item.img} title={item.title} desc={item.desc} price={item.price} key={index} />

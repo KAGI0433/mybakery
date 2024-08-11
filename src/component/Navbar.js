ef794ebcdca4182657c8d412658c0abfe3b9f610
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-08-11 17:06:34 +0200

    commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import logo from './Images/logo.png'
 import { Link } from "react-router-dom";
 import './Navbar.css'
+import { BiCart } from "react-icons/bi";
+import { BiUser } from "react-icons/bi";
 
 function Navbar() {
     return (
@@ -13,6 +15,16 @@ function Navbar() {
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
         <Link to= '/menu'>Menu</Link>
+        </div>
+
+        <div className="social-icon-cont">
+            <div className="headIcon">
+            <BiUser className="headIcon-size"/>
+            </div>
+            <div className="headIcon" >
+              <BiCart className="headIcon-size" />
+            </div>
+            
         </div>
 
         </div>
